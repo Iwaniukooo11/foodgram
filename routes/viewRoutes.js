006c7adc44b234de426cae650a555c5850c7ef3a
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2020-05-05 15:18:26 +0200

    add beta me template

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,5 +4,6 @@ const viewsController = require('../controllers/viewsController')
 const router = express.Router()
 router.route('/login').get(viewsController.getLogin)
 router.route('/signup').get(viewsController.getRegister)
+router.route('/me').get(viewsController.getMe)
 
 module.exports = router
