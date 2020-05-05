5a0ca7a54f0c656bd9fd7d4f0b88a9316e85886e
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2020-05-05 14:28:18 +0200

    finally done login and register

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,11 @@
 const catchAsync = require('./../utils/catchAsync')
 
 exports.getLogin = catchAsync(async (req, res) => {
-  res.status(200).render('login', {
-    title: 'uirra',
-  })
+  res.status(200).render('login', {})
+})
+exports.getRegister = catchAsync(async (req, res) => {
+  res.status(200).render('register', {})
+})
+exports.getMe = catchAsync(async (req, res) => {
+  res.status(200).render('me', {})
 })
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,5 +3,6 @@ const viewsController = require('../controllers/viewsController')
 
 const router = express.Router()
 router.route('/login').get(viewsController.getLogin)
+router.route('/signup').get(viewsController.getRegister)
 
 module.exports = router
